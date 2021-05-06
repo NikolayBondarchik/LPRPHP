fb0a1fa5824c7d9eb2db0e6b6a4a1b577e979ba7
NikolayBondarchik <73456147+NikolayBondarchik@users.noreply.github.com>
2021-05-06 15:10:28 +0600

    Add 06.05

--- mysite2.local/inc/gbook.inc.php
+++ mysite2.local/inc/gbook.inc.php
@@ -1,9 +1,30 @@
 <?php
-/* Основные настройки */
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['msg'])){
 
+/* Основные настройки */
+$name = $_POST['name'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
+
+$DB_HOST  = "localhost";
+$DB_NAME  = "gbook";
+$DB_LOGIN  = "root";
+$DB_PASSWORD  = "root";
+
+$conn = mysqli_connect($DB_HOST, $DB_LOGIN, $DB_PASSWORD, $DB_NAME);
 /* Основные настройки */
 
 /* Сохранение записи в БД */
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$sql = "INSERT INTO msgs (name, email, msg) VALUES ('$name', '$email', '$msg')";
+
+$result = mysqli_query($conn, $sql);
+
+}
 
 /* Сохранение записи в БД */
 
@@ -25,6 +46,28 @@ Email: <br /><input type="text" name="email" /><br />
 </form>
 <?php
 /* Вывод записей из БД */
+$name = $_POST['name'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
+
+$DB_HOST  = "localhost";
+$DB_NAME  = "gbook";
+$DB_LOGIN  = "root";
+$DB_PASSWORD  = "root";
+
+$conn = mysqli_connect($DB_HOST, $DB_LOGIN, $DB_PASSWORD, $DB_NAME);
+
+$sql = "SELECT id, name, email, msg, UNIX_TIMESTAMP(datetime) as dt
+FROM msgs
+ORDER BY id DESC";
+
+$result = mysqli_query($conn, $sql);
+
+$rows = mysqli_fetch_row($result)[0];
+
+echo "<p>Всего записей в гостевой книге: $rows</p>";
+
+
 
 /* Вывод записей из БД */
 ?>
